66fdfbe7a6c7a489bd83db2e27beeee25f51c1c6
Rohit Singh <rohitsinghpb4@gmail.com>
2024-06-13 16:42:53 +0530

    added database for added cart data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
-import { useSelector } from "react-redux";
 
 function App() {
     const showCart = useSelector((state) => state.ui.cartIsVisible);
+    const cart = useSelector((state) => state.cart);
+
+    useEffect(() => {
+        fetch("https://cart-data-b6116-default-rtdb.firebaseio.com/cart.json", {
+            method: "PUT",
+            body: JSON.stringify(cart),
+        });
+    }, [cart]);
     return (
         <Layout>
             {showCart && <Cart />}
