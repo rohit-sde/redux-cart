6b30db05b56e3a8d126a96e2b40ba84afc5b387e
Rohit Singh <rohitsinghpb4@gmail.com>
2024-06-14 00:06:40 +0530

    fetching our data fro database

--- src/store/cart-actions.js
+++ src/store/cart-actions.js
@@ -1,4 +1,36 @@
 import { uiActions } from "./ui-slice";
+import { cartActions } from "./cart-slice";
+
+export const fetchCartData = () => {
+    return async (dispatch) => {
+        const fetchData = async () => {
+            const response = await fetch(
+                "https://cart-data-b6116-default-rtdb.firebaseio.com/cart.json"
+            );
+
+            if (!response.ok) {
+                throw new Error("Could not fetch cart data!");
+            }
+
+            const data = await response.json();
+
+            return data;
+        };
+
+        try {
+            const cartData = await fetchData();
+            dispatch(cartActions.replaceCart(cartData));
+        } catch (error) {
+            dispatch(
+                uiActions.showNotification({
+                    status: "error",
+                    title: "Error!",
+                    message: "Fetching cart data failed!",
+                })
+            );
+        }
+    };
+};
 
 export const sendCartData = (cart) => {
     return async (dispatch) => {
